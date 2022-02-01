ff704343e6ed45f852fda3d89715b94a64ea42c9
NadaAlban <engnadaalban@gmail.com>
2022-02-01 18:34:43 +0300

    updated2

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -9,10 +9,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Admin Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="asset('vendor/fontawesome-free/css/all.min.css')" rel="stylesheet" type="text/css">
+      <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -155,6 +155,7 @@
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
           @yield('content')
+          {{-- رابط الى ملف الاندكس ادمن محتوى الصفحة --}}
         </div>
         <!-- End of Content Wrapper -->
 
